273f07726bbf061e4f218ce9424c57f807e56b47
ishigouoka_k <ishigouoka_k@DESKTOP-ELHNMUC>
2017-11-09 01:44:56 +0900

    test

--- src/main/java/com/Bacchus/app/controller/LoginController.java
+++ src/main/java/com/Bacchus/app/controller/LoginController.java
@@ -25,6 +25,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.Bacchus.app.components.AccessToken;
+import com.Bacchus.app.components.IdToken;
 import com.Bacchus.app.form.LoginNameForm;
 import com.Bacchus.app.service.LoggerService;
 import com.Bacchus.app.util.EncryptUtil;
@@ -37,6 +39,11 @@ import com.Bacchus.webbase.common.constants.MessageKeyConstants;
 import com.Bacchus.webbase.common.constants.SystemCodeConstants;
 import com.Bacchus.webbase.common.constants.SystemCodeConstants.MessageType;
 import com.Bacchus.webbase.common.constants.SystemCodeConstants.Permissions;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * ログイン用コントローラ。
@@ -105,11 +112,44 @@ public class LoginController extends BaseController {
         CloseableHttpClient httpClient = HttpClients.createDefault();
         HttpResponse httpResponse = httpClient.execute(httpPost);
 
-        String result = EntityUtils.toString(httpResponse.getEntity(), SystemCodeConstants.CHARACTER_ENCODING);
+        String json = EntityUtils.toString(httpResponse.getEntity(), SystemCodeConstants.CHARACTER_ENCODING);
 
-        System.out.println(result);
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode node = mapper.readTree(json);
+
+        AccessToken accessToken = new AccessToken(
+                node.get("scope").asText(),
+                node.get("access_token").asText(),
+                node.get("token_type").asText(),
+                Integer.valueOf(node.get("expires_in").asText()),
+                node.get("refresh_token").asText(),
+                node.get("id_token").asText()
+                );
+
+        IdToken idToken = idToken(accessToken.id_token);
+
+        System.out.println("aud:" + idToken.aud);
+        System.out.println("name:" + idToken.name);
+
 
         return "/";
+    }
+
+    public IdToken idToken(String id_token) {
+        try {
+            DecodedJWT jwt = JWT.decode(id_token);
+            return new IdToken(
+                    jwt.getClaim("iss").asString(),
+                    jwt.getClaim("sub").asString(),
+                    jwt.getClaim("aud").asString(),
+                    jwt.getClaim("ext").asLong(),
+                    jwt.getClaim("iat").asLong(),
+                    jwt.getClaim("nonce").asString(),
+                    jwt.getClaim("name").asString(),
+                    jwt.getClaim("picture").asString());
+        } catch (JWTDecodeException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @RequestMapping(method = RequestMethod.POST)

afc6015a8ff3adca61f0341ddb3cdb025795bfa0
145uoka <145uoka@gmail.com>
2018-01-06 17:30:51 +0900

    line push

--- src/main/java/com/Bacchus/app/service/LineService.java
+++ src/main/java/com/Bacchus/app/service/LineService.java
@@ -19,13 +19,13 @@ public class LineService {
     public void pushMessage(String accessToken) {
 
         LineMessagingClient lineMessagingClient = new LineMessagingClientBuilder(
-                "0e994f18b1e437b590bc5d25addcf1f5").build();
+                accessToken).build();
 
         try {
             BotApiResponse response = lineMessagingClient
                     .pushMessage(
                             new PushMessage(
-                                    accessToken,
+                                    "Ub0b37e268b2143fb40b997ca6a5b0820",
                                     new TemplateMessage(
                                             "Tomorrow is the garbage day for burnables！",
                                             new ConfirmTemplate("Did you take out the garbage?",

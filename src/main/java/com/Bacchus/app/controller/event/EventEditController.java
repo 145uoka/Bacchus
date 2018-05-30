a6029e9482bf1a3b6111cdf2f34caa629c9b9a00
145uoka <145uoka@gmail.com>
2018-05-31 02:48:38 +0900

    もろもろバグ修正

--- src/main/java/com/Bacchus/app/controller/event/EventEditController.java
+++ src/main/java/com/Bacchus/app/controller/event/EventEditController.java
@@ -26,7 +26,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.Bacchus.app.Exception.RecordNotFoundException;
-import com.Bacchus.app.components.CandidateDto;
 import com.Bacchus.app.components.EventDto;
 import com.Bacchus.app.components.LabelValueDto;
 import com.Bacchus.app.form.event.EventEditForm;
@@ -38,6 +37,7 @@ import com.Bacchus.app.service.event.EventCreateService;
 import com.Bacchus.app.service.event.EventService;
 import com.Bacchus.app.util.DateUtil;
 import com.Bacchus.app.util.MessageKeyUtil;
+import com.Bacchus.dbflute.exbhv.CandidateTBhv;
 import com.Bacchus.dbflute.exbhv.UserTBhv;
 import com.Bacchus.dbflute.exbhv.UserTypeMBhv;
 import com.Bacchus.dbflute.exentity.CandidateT;
@@ -60,7 +60,7 @@ import com.Bacchus.webbase.common.constants.SystemCodeConstants.MessageType;
 @RequestMapping(value = ProcConstants.EVENT)
 public class EventEditController extends BaseController {
 
-	 /** ロガーロジック */
+    /** ロガーロジック */
     @Autowired
     LoggerService loggerService;
 
@@ -85,183 +85,186 @@ public class EventEditController extends BaseController {
     @Autowired
     EntryService entryService;
 
-	 /**
+    @Autowired
+    CandidateTBhv candidateTBhv;
+
+    /**
      * イベント編集画面初期表示.
      *
      * @param model
      * @return "/event/eventEdit"
-	 * @throws UnsupportedEncodingException
+     * @throws UnsupportedEncodingException
      * @throws Exception
      */
-	@RequestMapping(value = ProcConstants.Operation.EDIT, method = RequestMethod.GET)
-	public String eventEdit(@ModelAttribute("form") EventEditForm form, BindingResult bindingResult,
-			RedirectAttributes redirectAttributes, Model model, HttpSession ses)
-			throws RecordNotFoundException, IllegalAccessException, InvocationTargetException, UnsupportedEncodingException {
+    @RequestMapping(value = ProcConstants.Operation.EDIT, method = RequestMethod.GET)
+    public String eventEdit(@ModelAttribute("form") EventEditForm form, BindingResult bindingResult,
+            RedirectAttributes redirectAttributes, Model model, HttpSession ses)
+                    throws RecordNotFoundException, IllegalAccessException, InvocationTargetException, UnsupportedEncodingException {
 
-		// 画面名の設定
-		super.setDisplayTitle(model, Event.BACCHUS_0203);
+        // 画面名の設定
+        super.setDisplayTitle(model, Event.BACCHUS_0203);
 
-		// イベント情報の取得
-		EventDto eventDto = eventService.findEventByPK(form.getEventNo());
+        // イベント情報の取得
+        EventDto eventDto = eventService.findEventByPK(form.getEventNo());
 
-		// record無し処理
-		if (eventDto == null) {
-			Map<String, Object> conditionMap = new HashMap<String, Object>();
-			conditionMap.put("eventNo", form.getEventNo());
-			throw new RecordNotFoundException("event_t", RecordNotFoundException.createKeyInfoMessage(conditionMap));
-		}
+        // record無し処理
+        if (eventDto == null) {
+            Map<String, Object> conditionMap = new HashMap<String, Object>();
+            conditionMap.put("eventNo", form.getEventNo());
+            throw new RecordNotFoundException("event_t", RecordNotFoundException.createKeyInfoMessage(conditionMap));
+        }
 
-		BeanUtils.copyProperties(eventDto, form);
-		if (eventDto.getEventTypeId() != null) {
-			// イベント種別の初期表示
-			form.setEventTypeId(String.valueOf(eventDto.getEventTypeId()));
-		}
-		if (eventDto.getUserId() != null) {
-			// 幹事の初期表示
-			form.setUserId(String.valueOf(eventDto.getUserId()));
-		}
-		if (eventDto.getEventEntryFee() != null) {
-			// 参加費の初期表示
-			form.setEventEntryFee(String.valueOf(eventDto.getEventEntryFee()));
-		}
-		if (eventDto.getAuxiliaryFlg() != null) {
-			// 経費補助有無の初期表示
-			form.setAuxiliaryFlg(String.valueOf(eventDto.getAuxiliaryFlg()));
-		}
+        BeanUtils.copyProperties(eventDto, form);
+        if (eventDto.getEventTypeId() != null) {
+            // イベント種別の初期表示
+            form.setEventTypeId(String.valueOf(eventDto.getEventTypeId()));
+        }
+        if (eventDto.getUserId() != null) {
+            // 幹事の初期表示
+            form.setUserId(String.valueOf(eventDto.getUserId()));
+        }
+        if (eventDto.getEventEntryFee() != null) {
+            // 参加費の初期表示
+            form.setEventEntryFee(String.valueOf(eventDto.getEventEntryFee()));
+        }
+        if (eventDto.getAuxiliaryFlg() != null) {
+            // 経費補助有無の初期表示
+            form.setAuxiliaryFlg(String.valueOf(eventDto.getAuxiliaryFlg()));
+        }
 
-		// 選択したイベント管理番号から、候補日_Tを取得
-		ListResultBean<CandidateT> candidateTList = entryService.findRegisterCandidateTList(form.getEventNo());
+        // 選択したイベント管理番号から、候補日_Tを取得
+        ListResultBean<CandidateT> candidateTList = candidateTBhv.selectList(cb->{
+            cb.query().setEventNo_Equal(form.getEventNo());
+            cb.query().addOrderBy_EventStartDatetime_Asc();
+        });
 
-		// record無し処理
-		if (CollectionUtils.isEmpty(candidateTList)) {
-			return ProcConstants.EVENT + ProcConstants.Operation.EDIT;
-		}
+        // record無し処理
+        if (CollectionUtils.isNotEmpty(candidateTList)) {
+            // 対象イベントの参加候補日の一覧を取得
+            List<String> candidateDtoList = new ArrayList<String>();
 
-		// 対象イベントの参加候補日の一覧を取得
-		List<String> candidateDtoList = new ArrayList<String>();
+            for (int i = 0; i < candidateTList.size(); i++) {
+                candidateDtoList.add(candidateTList.get(i).getStartDate());
 
-		for (int i = 0; i < candidateTList.size(); i++) {
-			CandidateDto candidateDto = eventService.convCandidateEntityToDto(candidateTList.get(i));
-			candidateDtoList.add(candidateDto.getStartDate());
+                // ラジオボタンの初期表示
+                if (candidateTList.get(i).getCandidateNo().equals(eventDto.getCandidateNo())) {
+                    // 候補日_Tの候補日管理番号とイベント＿Tの候補日管理番号が一致すれば、通し番号をラジオボタンにセット。
+                    form.setFixDate(String.valueOf(i));
+                }
+            }
 
-			// ラジオボタンの初期表示
-			if (candidateDto.getCandidateNo().equals(eventDto.getCandidateNo())) {
-				// 候補日_Tの候補日管理番号とイベント＿Tの候補日管理番号が一致すれば、通し番号をラジオボタンにセット。
-				form.setFixDate(String.valueOf(i));
-			}
-		}
+            if (eventDto.getCandidateNo() == null || eventDto.getCandidateNo() <= 0) {
+                // 候補日管理番号がnullであれば、ラジオボタンに""をセット。
+                form.setFixDate("");
+            }
 
-		if (eventDto.getCandidateNo() == null || eventDto.getCandidateNo() <= 0) {
-			// 候補日管理番号がnullであれば、ラジオボタンに""をセット。
-			form.setFixDate("");
-		}
+            form.setStartDate(candidateDtoList.toArray(new String[candidateDtoList.size()]));
+        }
 
-		form.setStartDate(candidateDtoList.toArray(new String[candidateDtoList.size()]));
+        model.addAttribute("form", form);
+        setPullDownList(model);
 
-		model.addAttribute("form", form);
-		setPullDownList(model);
+        return ProcConstants.EVENT + ProcConstants.Operation.EDIT;
+    }
 
-		return ProcConstants.EVENT + ProcConstants.Operation.EDIT;
-	}
+    /**
+     * イベント更新処理のコントローラー.
+     *
+     * @param form EventEditForm
+     * @param bindingResult BindingResult
+     * @param redirectAttributes RedirectAttributes
+     * @param model Model
+     * @return "/event/index"
+     * @throws Exception
+     */
+    @RequestMapping(value = ProcConstants.Operation.UPDATE, method = RequestMethod.POST)
+    public String store(@Validated @ModelAttribute("form") EventEditForm form, BindingResult bindingResult,
+            RedirectAttributes redirectAttributes, Model model) throws Exception {
 
-	/**
-    * イベント更新処理のコントローラー.
-    *
-    * @param form EventEditForm
-    * @param bindingResult BindingResult
-    * @param redirectAttributes RedirectAttributes
-    * @param model Model
-    * @return "/event/index"
-    * @throws Exception
-    */
-   @RequestMapping(value = ProcConstants.Operation.UPDATE, method = RequestMethod.POST)
-   public String store(@Validated @ModelAttribute("form") EventEditForm form, BindingResult bindingResult,
-           RedirectAttributes redirectAttributes, Model model) throws Exception {
+        super.setDisplayTitle(model, DisplayIdConstants.Event.BACCHUS_0203);
 
-	   super.setDisplayTitle(model, DisplayIdConstants.Event.BACCHUS_0203);
+        // form情報をModelへ格納。
+        model.addAttribute("form", form);
 
-	   // form情報をModelへ格納。
-	   model.addAttribute("form", form);
+        // 幹事のユーザIDの存在チェック
+        if (!bindingResult.hasFieldErrors("userId")) {
+            if (StringUtils.isNotEmpty(form.getUserId())) {
+                if (!isExistsUser(Integer.parseInt(form.getUserId()))) {
+                    bindingResult.rejectValue("userId",
+                            MessageKeyUtil.encloseStringDelete(GlueNetValidator.INVALID), null, "");
+                }
+            }
+        }
 
-       // 幹事のユーザIDの存在チェック
-       if (!bindingResult.hasFieldErrors("userId")) {
-           if (StringUtils.isNotEmpty(form.getUserId())) {
-               if (!isExistsUser(Integer.parseInt(form.getUserId()))) {
-                   bindingResult.rejectValue("userId",
-                           MessageKeyUtil.encloseStringDelete(GlueNetValidator.INVALID), null, "");
-               }
-           }
-       }
+        // 経費可否の妥当性チェック
+        if (!bindingResult.hasFieldErrors("auxiliaryFlg")) {
+            if (StringUtils.isNotEmpty(form.getAuxiliaryFlg())) {
+                if (!commonService.isExistsGenCode(SystemCodeConstants.GeneralCodeKbn.AUXILIARY_DIV,
+                        form.getAuxiliaryFlg())) {
+                    bindingResult.rejectValue("auxiliaryFlg",
+                            MessageKeyUtil.encloseStringDelete(GlueNetValidator.INVALID), null, "");
+                }
+            }
+        }
 
-       // 経費可否の妥当性チェック
-       if (!bindingResult.hasFieldErrors("auxiliaryFlg")) {
-           if (StringUtils.isNotEmpty(form.getAuxiliaryFlg())) {
-               if (!commonService.isExistsGenCode(SystemCodeConstants.GeneralCodeKbn.AUXILIARY_DIV,
-                       form.getAuxiliaryFlg())) {
-                   bindingResult.rejectValue("auxiliaryFlg",
-                           MessageKeyUtil.encloseStringDelete(GlueNetValidator.INVALID), null, "");
-               }
-           }
-       }
+        // イベント種別の妥当性チェック
+        if (!bindingResult.hasFieldErrors("eventTypeId")) {
+            if (StringUtils.isNotEmpty(form.getEventTypeId())) {
+                if (!eventService.isExistsEventType(Integer.parseInt(form.getEventTypeId()))) {
+                    bindingResult.rejectValue("eventTypeId",
+                            MessageKeyUtil.encloseStringDelete(GlueNetValidator.INVALID), null, "");
+                }
+            }
+        }
 
-       // イベント種別の妥当性チェック
-       if (!bindingResult.hasFieldErrors("eventTypeId")) {
-           if (StringUtils.isNotEmpty(form.getEventTypeId())) {
-               if (!eventService.isExistsEventType(Integer.parseInt(form.getEventTypeId()))) {
-                   bindingResult.rejectValue("eventTypeId",
-                           MessageKeyUtil.encloseStringDelete(GlueNetValidator.INVALID), null, "");
-               }
-           }
-       }
+        // 候補日の形式チェック
+        if (form.getStartDate() != null && form.getStartDate().length > 0) {
+            for (int i = 0; i < form.getStartDate().length; i++) {
+                String fieldName = "startDate[" + i + "]";
+                if (!bindingResult.hasFieldErrors(fieldName)) {
+                    if (StringUtils.isNotEmpty(form.getStartDate()[i])) {
+                        if (!DateUtil.isValidDateFormat(form.getStartDate()[i])) {
+                            bindingResult.rejectValue(fieldName,
+                                    MessageKeyUtil.encloseStringDelete(GlueNetValidator.DATEFORMAT_MESSAGE),
+                                    new String[]{DateUtil.DATE_TIME_FORMAT_YYYYMMDD}, "");
+                        }
+                    }
+                }
+            }
+        }
 
-       // 候補日の形式チェック
-       if (form.getStartDate() != null && form.getStartDate().length > 0) {
-           for (int i = 0; i < form.getStartDate().length; i++) {
-               String fieldName = "startDate[" + i + "]";
-               if (!bindingResult.hasFieldErrors(fieldName)) {
-                   if (StringUtils.isNotEmpty(form.getStartDate()[i])) {
-                       if (!DateUtil.isValidDateFormat(form.getStartDate()[i])) {
-                           bindingResult.rejectValue(fieldName,
-                                   MessageKeyUtil.encloseStringDelete(GlueNetValidator.DATEFORMAT_MESSAGE),
-                                   new String[]{DateUtil.DATE_TIME_FORMAT_YYYYMMDD}, "");
-                       }
-                   }
-               }
-           }
-       }
+        // 確定ボタンを選択した候補日が空白でないかの判定 空白ならtrue
+        if (eventService.isFixCandidate(form)) {
 
-       // 確定ボタンを選択した候補日が空白でないかの判定 空白ならtrue
-       if (eventService.isFixCandidate(form)) {
+            // エラー文のセット
+            bindingResult.rejectValue("startDate[" + Integer.parseInt(form.getFixDate()) + "]",
+                    MessageKeyUtil.encloseStringDelete(GlueNetValidator.NOTBLANK_WITH_FIELD),
+                    new Object[] { "確定対象の日付" }, "");
+        }
 
-           // エラー文のセット
-           bindingResult.rejectValue("startDate[" + Integer.parseInt(form.getFixDate()) + "]",
-                   MessageKeyUtil.encloseStringDelete(GlueNetValidator.NOTBLANK_WITH_FIELD),
-                   new Object[] { "確定対象の日付" }, "");
-       }
+        // validation確認
+        if (bindingResult.hasErrors()) {
 
-       // validation確認
-       if (bindingResult.hasErrors()) {
+            model.addAttribute(MODEL_KEY_FORM, form);
+            model.addAttribute("errors", bindingResult);
 
-           model.addAttribute(MODEL_KEY_FORM, form);
-           model.addAttribute("errors", bindingResult);
+            setPullDownList(model);
 
-           setPullDownList(model);
+            return ProcConstants.EVENT + ProcConstants.Operation.EDIT;
+        }
 
-           return ProcConstants.EVENT + ProcConstants.Operation.EDIT;
-       }
+        // DB更新
+        eventService.update(form);
 
-       // DB更新
-       eventService.update(form);
+        // 完了メッセージを設定
+        String message = messageSource.getMessage(
+                MessageKeyUtil.encloseStringDelete(MessageKeyConstants.Success.UPDATE), null, Locale.getDefault());
 
-       // 完了メッセージを設定
-       String message = messageSource.getMessage(
-               MessageKeyUtil.encloseStringDelete(MessageKeyConstants.Success.UPDATE), null, Locale.getDefault());
+        List<String> successMessageList = new ArrayList<String>(Arrays.asList(message));
+        redirectAttributes.addFlashAttribute(MessageType.SUCCESS, successMessageList);
 
-       List<String> successMessageList = new ArrayList<String>(Arrays.asList(message));
-       redirectAttributes.addFlashAttribute(MessageType.SUCCESS, successMessageList);
-
-       return redirect(ProcConstants.EVENT + ProcConstants.Operation.INDEX);
-   }
+        return redirect(ProcConstants.EVENT + ProcConstants.Operation.INDEX);
+    }
 
     /**
      * プルダウン項目の設定
